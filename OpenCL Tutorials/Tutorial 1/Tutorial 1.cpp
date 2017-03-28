53fdd373338be73607a51a881ce175aa2556c02d
Max Cussans <max-cussans2@hotmail.co.uk>
2017-03-28 15:52:03 +0100

    global max first attempt

--- OpenCL Tutorials/Tutorial 1/Tutorial 1.cpp
+++ OpenCL Tutorials/Tutorial 1/Tutorial 1.cpp
@@ -38,12 +38,14 @@ float* read(const char* filepath, int length)
 
 
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 	//Part 1 - handle command line options such as device selection, verbosity, etc.
 	int platform_id = 0;
 	int device_id = 0;
 
-	for (int i = 1; i < argc; i++)	{
+	for (int i = 1; i < argc; i++)	
+	{
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
 		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
 		else if (strcmp(argv[i], "-l") == 0) { cout << ListPlatformsDevices() << endl; }
@@ -83,54 +85,44 @@ int main(int argc, char **argv) {
 
 		//Part 4 - memory allocation
 		//host - input
-		vector<int> A = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //C++11 allows this type of initialisation
-		vector<int> B = { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0 };
 
-		int datasize = 1873106;
+
+		const int datasize = 1873106;
 		char* filepath = "temp_lincolnshire.txt";
 		float* temperatures;
 		temperatures = read(filepath, datasize);
 		
-		size_t vector_elements = A.size();//number of elements
-		size_t vector_size = A.size()*sizeof(int);//size in bytes
+		size_t vector_elements = datasize;//number of elements
+		size_t vector_size = datasize*sizeof(int);//size in bytes
 
 		//host - output
-		vector<int> C(vector_elements);
 
 		//device - buffers
 		cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, vector_size);
-		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, vector_size);
-		cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, vector_size);
+
 
 		//Part 5 - device operations
 
 		//5.1 Copy arrays A and B to device memory
-		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, vector_size, &A[0]);
-		queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, vector_size, &B[0]);
+		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, vector_size, &temperatures[0]);
 
 		//5.2 Setup and execute the kernel (i.e. device code)
-		cl::Kernel kernel_add = cl::Kernel(program, "add");
+		cl::Kernel kernel_add = cl::Kernel(program, "global_max");
 		kernel_add.setArg(0, buffer_A);
-		kernel_add.setArg(1, buffer_B);
-		kernel_add.setArg(2, buffer_C);
+		kernel_add.setArg(1, buffer_A);
 
 		queue.enqueueNDRangeKernel(kernel_add, cl::NullRange, cl::NDRange(vector_elements), cl::NullRange);
 
-		//5.3 Copy the result from device to host
-		queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, vector_size, &C[0]);
 
-		cout << "A = " << A << endl;
-		cout << "B = " << B << endl;
-		cout << "C = " << C << endl;
-		for (int i = 0; i < datasize; i++)
-		{
-			cout << temperatures[i] << endl;
-		}
+		
+			cout << temperatures[0] << endl;
+		
 	}
 	catch (cl::Error err) {
 		cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << endl;
 	}
 
+	system("PAUSE");
 	return 0;
 }
 
--- OpenCL Tutorials/Tutorial 1/my_kernels.cl
+++ OpenCL Tutorials/Tutorial 1/my_kernels.cl
@@ -1,7 +1,38 @@
-//a simple OpenCL kernel which adds two vectors A and B together into a third vector C
 
-__kernel void add(__global const float* A, __global const float* B, __global float* C)
+__kernel void reduce_add(__global float* A) 
 {
 	int id = get_global_id(0);
-	C[id] = A[id] + B[id];
+	int N = get_global_size(0);
+
+	for (int i = 1; i < N; i*=2) 
+	{ 
+		if (id % (i*2) == 0)
+		{ 	
+			A[id] += A[id+i];
+		}
+		barrier(CLK_GLOBAL_MEM_FENCE);
+	}
 }
+
+__kernel void global_max(__global int* A, __global int* B)
+{
+	int id = get_global_id(0);
+	int N = get_global_size(0);
+
+	B[id] = A[id];
+	
+	barrier(CLK_GLOBAL_MEM_FENCE); //ensure all values are copied before continuing
+
+	for(int i =1; i < N; i++)
+	{
+		if(A[i + 1] > B[0])
+		{
+			B[0] = A[i + 1];
+		}
+
+		barrier(CLK_GLOBAL_MEM_FENCE);
+	}
+
+}
+
+
